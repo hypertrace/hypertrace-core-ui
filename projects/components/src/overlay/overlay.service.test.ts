1916f5bc58749b7ff93a11ad81090ca06f81cc30
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2020-08-07 14:07:55 -0700

    test: adding tests

--- projects/components/src/overlay/overlay.service.test.ts
+++ projects/components/src/overlay/overlay.service.test.ts
@@ -5,6 +5,7 @@ import { recordObservable, runFakeRxjs } from '@hypertrace/test-utils';
 import { createServiceFactory, mockProvider, SpectatorService } from '@ngneat/spectator/jest';
 import { Subject } from 'rxjs';
 import { PopoverModule } from '../popover/popover.module';
+import { ModalSize } from './modal/modal';
 import { OverlayService } from './overlay.service';
 import { SheetSize } from './sheet/sheet';
 
@@ -27,10 +28,36 @@ describe('Overlay service', () => {
     spectator = createService();
   });
 
-  test('can close a popover on navigation', fakeAsync(() => {
+  test('can close a sheet popover on navigation', fakeAsync(() => {
     const popover = spectator.service.createSheet({
       showHeader: false,
       size: SheetSize.Medium,
+      content: Component({
+        selector: 'test-component',
+        template: `<div>TEST</div>`
+      })(class {})
+    });
+    popover.show();
+    popover.closeOnNavigation();
+    tick(); // CDK overlay is async
+
+    expect(popover.closed).toBe(false);
+
+    runFakeRxjs(({ expectObservable }) => {
+      expectObservable(popover.closed$).toBe('(x|)', { x: undefined });
+      expectObservable(recordObservable(popover.hidden$)).toBe('|'); // Record hidden/shown for test, since they're hot
+      expectObservable(recordObservable(popover.shown$)).toBe('|');
+      navigation$.next();
+    });
+
+    expect(popover.closed).toBe(true);
+    flush(); // CDK cleans up overlay async
+  }));
+
+  test('can close a modal popover on navigation', fakeAsync(() => {
+    const popover = spectator.service.createModal({
+      showHeader: false,
+      size: ModalSize.Small,
       content: Component({
         selector: 'test-component',
         template: `<div>TEST</div>`
