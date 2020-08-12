7b2f6037e3009f2bb59037a95301de4b9cab5567
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2020-08-12 12:30:06 -0700

    test: adding tests

---
+++ projects/distributed-tracing/src/shared/dashboard/interaction/detail-sheet/detail-sheet-interaction-handler.service.test.ts
@@ -0,0 +1,34 @@
+import { OverlayService, SheetSize } from '@hypertrace/components';
+import { createServiceFactory, mockProvider, SpectatorService } from '@ngneat/spectator/jest';
+import { DetailSheetInteractionContainerComponent } from './container/detail-sheet-interaction-container.component';
+import { DetailSheetInteractionHandlerService } from './detail-sheet-interaction-handler.service';
+
+describe('Overlay service', () => {
+  let spectator: SpectatorService<DetailSheetInteractionHandlerService>;
+
+  const createService = createServiceFactory({
+    service: DetailSheetInteractionHandlerService,
+    providers: [
+      mockProvider(OverlayService, {
+        createSheet: jest.fn()
+      })
+    ]
+  });
+
+  beforeEach(() => {
+    spectator = createService();
+  });
+
+  test('should call overlay service', () => {
+    spectator.service.showSheet({}, SheetSize.Medium);
+    expect(spectator.inject(OverlayService).createSheet).toHaveBeenCalledWith(
+      {
+        content: DetailSheetInteractionContainerComponent,
+        size: SheetSize.Medium
+      },
+      expect.objectContaining({
+        /* Injector */
+      })
+    );
+  });
+});
