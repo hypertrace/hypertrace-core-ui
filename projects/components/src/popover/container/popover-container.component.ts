277664229cfab75bc01bbd430896cb36d927a487
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2020-08-19 16:13:34 -0700

    fix: fixing detail overlay issues
    - injecting Layout change service in popover
    - adding a model property to hide header

--- projects/components/src/popover/container/popover-container.component.ts
+++ projects/components/src/popover/container/popover-container.component.ts
@@ -1,17 +1,26 @@
-import { ChangeDetectionStrategy, Component, Inject, InjectionToken, Injector, TemplateRef, Type } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  InjectionToken,
+  Injector,
+  TemplateRef,
+  Type,
+  ElementRef
+} from '@angular/core';
 import { POPOVER_DATA } from '../popover';
+import { LayoutChangeService } from '@hypertrace/common';
 
 export const POPOVER_CONTAINER_DATA = new InjectionToken<PopoverContainerData>('POPOVER_CONTAINER_DATA');
 
 @Component({
   selector: 'htc-popover-container',
   styleUrls: ['./popover-container.component.scss'],
+  providers: [LayoutChangeService], // Provided here since popovers are in detached state
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <ng-container *ngIf="this.isComponentRenderer; else templateRenderer">
-      <ng-container
-        *ngComponentOutlet="this.containerData.popoverRenderer; injector: this.containerData.popoverInjector"
-      >
+      <ng-container *ngComponentOutlet="this.containerData.popoverRenderer; injector: this.componentInjector">
       </ng-container>
     </ng-container>
     <ng-template #templateRenderer>
@@ -23,13 +32,30 @@ export const POPOVER_CONTAINER_DATA = new InjectionToken<PopoverContainerData>('
 export class PopoverContainerComponent {
   public readonly isComponentRenderer: boolean;
   public readonly templateContext: unknown;
+  public readonly componentInjector: Injector;
   public constructor(
     @Inject(POPOVER_CONTAINER_DATA)
-    public readonly containerData: PopoverContainerData
+    public readonly containerData: PopoverContainerData,
+    hostElementRef: ElementRef,
+    layoutChangeService: LayoutChangeService
   ) {
     this.isComponentRenderer = !(containerData.popoverRenderer instanceof TemplateRef);
     // tslint:disable-next-line: no-null-keyword Required so angular doesn't default to throw
     this.templateContext = this.containerData.popoverInjector.get(POPOVER_DATA, null);
+
+    this.componentInjector = Injector.create({
+      providers: [
+        {
+          provide: ElementRef,
+          useValue: hostElementRef
+        },
+        {
+          provide: LayoutChangeService,
+          useValue: layoutChangeService
+        }
+      ],
+      parent: this.containerData.popoverInjector
+    });
   }
 }
 
