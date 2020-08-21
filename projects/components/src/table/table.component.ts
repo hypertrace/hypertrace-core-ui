23795f7857d45e69ff646321d45170c43212852c
jake-bassett <45181984+jake-bassett@users.noreply.github.com>
2020-08-21 13:11:34 -0700

    Update projects/components/src/table/table.component.ts
    Co-authored-by: Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>

--- projects/components/src/table/table.component.ts
+++ projects/components/src/table/table.component.ts
@@ -312,7 +312,7 @@ export class TableComponent
   public ngAfterViewInit(): void {
     setTimeout(() => {
       this.initializeData();
-      this.changeDetector.detectChanges();
+      this.changeDetector.markForCheck();
     });
   }
 
